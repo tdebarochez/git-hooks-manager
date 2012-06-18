f2e1e6dca7e27e8d5acff8a11130b3d0f2aaf37f
Thomas Debarochez <thomas.barochez+github@gmail.com>
2012-06-18 08:53:35 +0200

    add async option, clean-up and better working directory usage

--- index.js
+++ index.js
@@ -4,23 +4,16 @@ var fs = require('fs')
   , path = require('path')
   , child_process = require('child_process');
 
-var hooks = ['post-commit', 'pre-commit', 'post-receive'];
+var hooks = ['post-commit', 'pre-commit', 'post-receive']
+  , root_path = path.join('.git', 'hooks');
 
 function Manager () {
-  this.getHooksConf = function (hook_type, hook_name, cb) {
+  this.getHooksConf = function (opts, cb) {
     var glob = require('glob')
-      , confs = [];
-    if (typeof hook_name !== "function") {
-      var conf_file = path.join('hooks', hook_type, hook_name, 'hook.json');
-      fs.exists(conf_file, function (exists) {
-        if (!exists) {
-          return cb("hook configuration not found");
-        }
-        cb(null, require('./' + conf_file));
-      });
-      return;
-    }
-    cb = hook_name;
+      , confs = []
+      , hook_type = "hook_type" in opts ? opts.hook_type : opts
+      , hook_name = opts.hook_name
+      , root_path = "root_path" in opts ? opts.root_path : '.';
     fs.stat(hook_type + '.d', function(err, stat) {
       if (err) {
         return console.error(err);
@@ -28,7 +21,17 @@ function Manager () {
       if (!stat.isDirectory()) {
         return console.error('hook ' + hook_type + ' not found');
       }
-      glob(path.join('hooks', hook_type, '*', 'hook.json'), function (err, files) {
+      if (hook_name) {
+        var conf_file = path.join(root_path, 'hooks', hook_type, hook_name, 'hook.json');
+        fs.exists(conf_file, function (exists) {
+          if (!exists) {
+            return cb("hook configuration not found");
+          }
+          cb(null, require('./' + conf_file));
+        });
+        return;
+      }
+      glob(path.join(root_path, 'hooks', hook_type, '*', 'hook.json'), function (err, files) {
         if (err) {
           return cb(err);
         }
@@ -62,50 +65,67 @@ Manager.prototype = {
   hook: function (hook_type) {
     var daemon = require('daemon')
       , glob = require('glob');
-    var inputs = "";
+    var inputs = ""
+      , daemonized = true
+      , self = this;
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
     process.stdin.on('data', function (chunk) {
       inputs += chunk;
     });
     process.stdin.on('end', function () {
-      var pid = daemon.start('stdout.log', 'stderr.log');
-      console.log(new Date + ' : background process started successfully with pid ' + pid);
-      glob(path.join(hook_type + '.d', '*'), function (err, files) {
+      glob(path.join(root_path, hook_type + '.d', '*'), function (err, files) {
         if (err) {
           return console.log(err);
         }
         console.log(files.length + ' script(s) to execute, ', files);
-        files.forEach(function (filename) {
-          if (fs.statSync(filename).isDirectory()) {
+        files.forEach(function (hook_name) {
+          if (fs.statSync(hook_name).isDirectory()) {
             return;
           }
-          var child_proc = child_process.execFile(filename, [], {}, function (err) {
+          self.getHooksConf({"hook_type": hook_type,
+                             "hook_name": hook_name,
+                             "root_path": root_path}, function (err, conf) {
             if (err) {
-              console.error(filename + ' execution failed');
+              return console.error(err);
+            }
+            if ((("async" in conf && conf.async)
+                 || (!("async" in conf) && hook_type.substr(0, 5) == 'post-'))
+                && !daemonized) {
+              daemonized = true;
+              var pid = daemon.start(path.join(root_path, 'stdout.log'), path.join(root_path, 'stderr.log'));
+              console.log(new Date + ' : background process started successfully with pid ' + pid);
+            }
+            var child_proc = child_process.execFile(hook_name, [], {}, function (err) {
+              if (err) {
+                console.error(hook_name + ' execution failed');
+                console.error('' + err);
+                child_proc.stdin.end();
+                return;
+              }
+            });
+            console.log('execute ' + hook_name + ' (' + child_proc.pid + ')');
+            if (child_proc.stdout.readable) {
+              child_proc.stdout.pipe(process.stdout);
+            }
+            if (child_proc.stderr.readable) {
+              child_proc.stderr.pipe(process.stderr);
+            }
+            child_proc.stdin.on('error', function (err) {
               console.error('' + err);
+            });
+            if (child_proc.stdin.writable) {
+              child_proc.stdin.write(inputs);
               child_proc.stdin.end();
-              return;
             }
           });
-          console.log('execute ' + filename + ' (' + child_proc.pid + ')');
-          if (child_proc.stdout.readable) {
-            child_proc.stdout.pipe(process.stdout);
-          }
-          child_proc.stdin.on('error', function (err) {
-            console.error('' + err);
-          });
-          if (child_proc.stdin.writable) {
-            child_proc.stdin.write(inputs);
-            child_proc.stdin.end();
-          }
         });
       });
     });
   },
   search: function (hook_type, query) {
     var hooks = [];
-    module.exports.getHooksConf(hook_type, function (err, confs) {
+    this.getHooksConf(hook_type, function (err, confs) {
       if (err) {
         return console.error(err);
       }
@@ -123,16 +143,17 @@ Manager.prototype = {
     })
   },
   add: function (hook_type, hook_name) {
-    this.getHooksConf(hook_type, hook_name, function (err, conf) {
+    this.getHooksConf({"hook_type": hook_type,
+                       "hook_name": hook_name}, function (err, conf) {
       if (err) {
         return console.error(err);
       }
-      var filepath = path.join(hook_type + '.d', hook_name + '.index');
+      var filepath = path.join(hook_type + '.d', hook_name);
       if (fs.existsSync(filepath)) {
         return console.error(hook_name + ' already exists for the ' + hook_type + ' hook');
       }
       function setup () {
-        fs.symlinkSync(path.join('..', 'hooks', hook_type, hook_name, conf.index), path.join(hook_type + '.d', hook_name + '.index'));
+        fs.symlinkSync(path.join('..', 'hooks', hook_type, hook_name, conf.index), path.join(hook_type + '.d', hook_name));
         console.log('hook setup');
         if ('post-install' in conf) {
           var args = {cwd: path.join('hooks', hook_type, hook_name)};
@@ -170,7 +191,8 @@ Manager.prototype = {
     });
   },
   rm: function (hook_type, hook_name) {
-    this.getHooksConf(hook_type, hook_name, function (err, conf) {
+    this.getHooksConf({"hook_type": hook_type,
+                       "hook_name": hook_name}, function (err, conf) {
       if (err) {
         return console.error(err);
       }
@@ -194,8 +216,7 @@ Manager.prototype = {
           });
         }
       }
-      var directory = path.join(hook_type + '.d', hook_name)
-        , index = path.join(hook_type + '.d', hook_name + '.index');
+      var index = path.join(hook_type + '.d', hook_name);
       if ('pre-remove' in conf) {
         var args = {cwd: path.join('hooks', hook_type, hook_name)};
         child_process.exec(conf['pre-remove'], args, function (err, stdout, stderr) {
